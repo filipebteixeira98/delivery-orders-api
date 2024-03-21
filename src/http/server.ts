d70048044c02b66839e0e4d91c381580850f3410
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-03-21 00:21:43 -0300

    refactor(app): to add register restaurant route file

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,43 +1,8 @@
-/* eslint-disable */
+import { Elysia } from 'elysia'
 
-import { Elysia, t } from 'elysia'
+import { registerRestaurant } from './routes/register-restaurant'
 
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
-
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({
-        id: users.id,
-      })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    })
-
-    set.status = 204
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      phone: t.String(),
-      email: t.String({ format: 'email' }),
-    }),
-  }
-)
+const app = new Elysia().use(registerRestaurant)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server is running at localhost:3333')
